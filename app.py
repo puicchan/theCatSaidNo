2d3ec4bdf8acaab0d9c2f2be77214d5eea1025ef
puicchan <puicchan>
2022-11-02 21:08:41 -0700

    Edits for part2

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, render_template, send_from_directory
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+
+INSTRUMENTATION_KEY = os.environ.get('APPLICATIONINSIGHTS_CONNECTION_STRING')
 
 app = Flask(__name__)
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string=INSTRUMENTATION_KEY),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 
 @app.route('/favicon.ico')
 def favicon():
